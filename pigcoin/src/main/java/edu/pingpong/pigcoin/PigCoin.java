a158b118b474d41a66f21f9a915605c64a577c5a
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-02-23 09:22:53 +0100

    Creación proyecto

---
+++ pigcoin/src/main/java/edu/pingpong/pigcoin/PigCoin.java
@@ -0,0 +1,13 @@
+package edu.pingpong.pigcoin;
+
+/**
+ * Hello world!
+ *
+ */
+public class PigCoin 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
